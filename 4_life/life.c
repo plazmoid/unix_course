827ff64af15edd994d45a147e859554f5c6e8e1b
plazmoid <kronos44@mail.ru>
2020-04-07 11:38:27 +0500

    implemented syncronisation

--- 4_life/life.c
+++ 4_life/life.c
@@ -4,9 +4,11 @@
 #include <stdbool.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <fcntl.h>
+#include <time.h>
 #include "life.h"
 #include "datatypes.h"
 
@@ -44,9 +46,12 @@ int errwrap(int ret) {
 }
 
 void sock_init() {
+    int opt = 1;
     listen_socket_fd = errwrap(socket(AF_INET, SOCK_STREAM, 0));
     int flags = errwrap(fcntl(listen_socket_fd, F_GETFL));
     errwrap(fcntl(listen_socket_fd, F_SETFL, flags | O_NONBLOCK));
+    errwrap(setsockopt(listen_socket_fd, SOL_SOCKET, SO_REUSEADDR, 
+            &opt, sizeof(int)));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(31337);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -78,30 +83,13 @@ void game_of_life(CellField *cfield) {
     ActionTuple *action, *fld_actions;
     int neighbours = 0;
     int fld_actions_len = 0;
+    bool sync_ready = true;
+    int sync_fd;
+    time_t real_time = 0, delta = 0;
+    struct stat file_stat;
     fld_actions = malloc(cfield->fx * cfield->fy);
     while(1) {
         fld_actions_len = 0;
-        /*///////////////
-        system("clear");
-        printf("  ");
-        for(int x = 0; x < cfield->fx; x++) {
-            printf("%d", x);
-        }
-        printf("\n");
-        for(int y = 0; y < cfield->fy; y++) {
-            printf("%d ", y);
-            for(int x = 0; x < cfield->fx; x++) {
-                char c = get_cell(cfield, x, y);
-                printf("%c", c);
-            }
-            printf("\n");
-        }
-        printf("  ");
-        for(int x = 0; x < cfield->fx; x++) {
-            printf("%d", x);
-        }
-        printf("\n");
-        ////////////////*/
         for(int x = 0; x < cfield->fx; x++) {
             for(int y = 0; y < cfield->fy; y++) {
                 neighbours = count_neighbours(cfield, x, y);
@@ -135,8 +123,26 @@ void game_of_life(CellField *cfield) {
                 set_cell(cfield, action->fx, action->fy, NO_CELL);
             }
         }
-        client_handler(cfield);
-        sleep(1);
+        sync_ready = false;
+        sync_fd = creat(SYNCFILE, 0666);
+        if(sync_fd == -1) {
+            err(NULL, SYNCFILE, true);
+        }
+        while(!sync_ready) {
+            client_handler(cfield);
+            errwrap(stat(SYNCFILE, &file_stat));
+            time(&real_time);
+            delta = real_time - file_stat.st_mtime;
+            if(delta == 1) {
+                sync_ready = true;
+            } else if(delta > 1) {
+                char delta_val[32];
+                sprintf(delta_val, "%ld", delta);
+                err(ERR_LONG_TICK, delta_val, false);
+                sync_ready = true;
+            }
+            usleep(500);
+        }
     }
     free(fld_actions);
 }
@@ -161,8 +167,6 @@ int main(int argc, char **argv) {
     for(int row = 0; row < cfield.fy; row++) {
         cfield.field[row] = (char*)calloc(1, cfield.fx);
         fscanf(input, "%s\n", cfield.field[row]);
-        DBG("reading row %d\n", row);
-        //DBG("row: %s\n", cfield.field[row]);
         if(ferror(input) > 0) {
             err(NULL, NULL, true);
         }
@@ -186,5 +190,6 @@ int main(int argc, char **argv) {
         free(cfield.field[row]);
     }
     free(cfield.field);
+    close(listen_socket_fd);
     return 0;
 }
--- 4_life/life.h
+++ 4_life/life.h
@@ -1,10 +1,11 @@
-#define SOCK "/tmp/life.sock"
+#define SYNCFILE "/tmp/life"
 #define INITFILE "init.txt"
 #define FIELD_MAX_X 100
 #define FIELD_MAX_Y 100
 
 #define ERR_TOO_LARGE "Field is too big, try to reduce x or y in init file"
 #define ERR_WRONG_CELL_VAL "Wrong cell value at (x:%d, y:%d)"
+#define ERR_LONG_TICK "Last update took more than 1 second"
 
 #define CELL 'o'
 #define NO_CELL '.'
