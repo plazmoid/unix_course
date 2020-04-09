b769016858dbae3390b248ae1bffd922d5915350
plazmoid <kronos44@mail.ru>
2020-04-10 04:22:45 +0500

    added all

--- 6_locks/locks.c
+++ 6_locks/locks.c
@@ -5,38 +5,90 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 #include "locks.h"
+
+void err(char *msg, const char *arg, bool critical) {
+    if(msg == NULL || strlen(msg) == 0) {
+        msg = strerror(errno);
+    }
+    if (arg != NULL) {
+        fprintf(stderr, "%s, '%s'\n", msg, arg);
+    } else {
+        fprintf(stderr, "%s\n", msg);
+    }
+    if(critical) {
+        exit(-1);
+    }
+}
 
 int errwrap(int ret) {
     if(ret == -1) {
-        printf("%s\n", strerror(errno));
-        exit(-1);
+        err(NULL, NULL, true);
     }
     return ret;
 }
 
 int manage_lock(char *file, lock_t l_type, bool acq_lock) {
     int lfd;
-    char *path[512];
+    char lck_path[512];
+    char pid[16];
     bool locked; 
     
-    sprintf(path, "%s/%s.%s", LCK_DIR, file, LCK_SUFFIX);
-    locked = access(path, F_OK) == 0;
+    sprintf(lck_path, "%s/%s.%s", LCK_DIR, file, LCK_SUFFIX);
+    locked = access(lck_path, F_OK) == 0;
     if(locked) {
         if(acq_lock) {
             switch (l_type) {
-
                 case L_WR:
-                    /* code */
-                    break;
+                    return LE_LOCKD;
+
+                case L_RD:
+                    return 0;
+
+                default:
+                    err(ERR_UNK, NULL, false);
+                    return LE_SOME;
             }
-            lfd = errwrap(open(path, O_CREAT));
+        } else {
+            char read_pid[16];
+            lfd = errwrap(open(lck_path, O_RDONLY));
+            errwrap(read(lfd, read_pid, 15));
+            close(lfd);
+            sprintf(pid, "%d", (int)getpid());
+            if(strncmp(pid, read_pid, strlen(pid)) == 0) {
+                return unlink(lck_path);
+            }
+            return LE_PID_NE;
         }
     } else {
         if(acq_lock) {
+            lfd = errwrap(open(lck_path, O_CREAT | O_WRONLY));
+            sprintf(pid, "%d\n", (int)getpid());
+            errwrap(write(lfd, pid, strlen(pid)));
+            switch (l_type) {
+                case L_WR:
+                    if(write(lfd, "w", 1) == -1) {
+                        close(lfd);
+                        return LE_SOME;
+                    }
+                    break;
+                case L_RD:
+                    if(write(lfd, "r", 1) == -1) {
+                        close(lfd);
+                        return LE_SOME;
+                    }
+                    break;
 
+                default:
+                    err(ERR_UNK, NULL, false);
+                    close(lfd);
+                    return LE_SOME;
+            }
+            close(lfd);
+            return 0;
         } else {
-
+            return 0;
         }
     }
 }
@@ -50,9 +102,9 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     file = argv[1];
-    while(lock(file, L_WR, true) != 0) {
+    while(manage_lock(file, L_WR, true) != 0) {
         if(!errord) {
-            printf("%s\n", ERR_LOCKD);
+            err(ERR_LOCKD, NULL, false);
             errord = true;
         }
         usleep(1000);
@@ -63,11 +115,13 @@ int main(int argc, char *argv[]) {
     }
     pid = errwrap(fork());
     if(pid == 0) {
-        execlp(editor, editor, file);
+        execlp(editor, editor, file, (char*)NULL);
         exit(-1);
     } else {
-        waitpid(pid);
+        waitpid(pid, NULL, 0);
+        if(manage_lock(file, L_WR, false) != 0) {
+            err(ERR_RELEASE, file, false);
+        }
     }
-    lock(file, L_WR, false);
     return 0;
 }
--- 6_locks/locks.h
+++ 6_locks/locks.h
@@ -4,8 +4,16 @@
 #define LCK_SUFFIX "lck"
 
 #define ERR_LOCKD "This file is already locked, waiting for lock release"
+#define ERR_UNK "Unknown lock type"
+#define ERR_RELEASE "Failed to release lock"
 
 typedef enum {
     L_WR = O_WRONLY,
     L_RD = O_RDONLY
 } lock_t;
+
+typedef enum {
+    LE_PID_NE = 1,
+    LE_LOCKD,
+    LE_SOME
+} lock_err;
